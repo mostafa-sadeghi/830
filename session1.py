2815d454a5f25b7227812cbd6d047ad3caf0d9a8
msa <msai.sadehi@gmail.com>
2023-07-02 15:56:00 +0330

    sadeghi

--- session1.py
+++ session1.py
@@ -1,1 +1,23 @@
 print("hello every body")
+#  سلام بچه ها
+print(1 + 2)
+print("1 + 2")
+
+print(1 + 2 * 3)
+print((1 + 2) * 3)
+
+#  +    -    *    /     %     //
+
+
+x = 12
+print(x)
+print(type(x))
+
+
+number = 1.5
+print(type(number))
+
+
+name = "artin"
+print(name)
+print(type(name))
