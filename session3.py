c2bd7ab386acc69145a70c6a2dc114c299e4f317
msa <msai.sadehi@gmail.com>
2023-07-06 15:56:08 +0330

    second session

---
+++ session3.py
@@ -0,0 +1,37 @@
+# name = "ayda"
+# print(name)
+# print(type(name))
+
+
+# message = "hello every body. \nwelcome to python class. \nwe are going to learn every things about python."
+# print(message)
+
+# message = """hello every body.
+# welcome to python class.
+# we are going to learn every things about python."""
+# print(message)
+
+# message = '''hello every body.
+# welcome to python class.
+# we are going to learn every things about python.'''
+# print(message)
+
+
+# name = "sara"
+# message = "Hello " + name
+# print(message)
+
+# message = f"Hello {name}"
+# print(message)
+
+name = input("Enter your name: ")
+print(name)
+
+family = input("enter your family: ")
+print(family)
+
+
+# TODO برنامه ای بنویسید که نام و نام خانوادگی دانش آموزی را از وروید دریافت نماید
+# و پیغامی شبیه زیر نمایش دهد
+
+# amirali asadi is a python student
