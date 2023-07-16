41dacd2c4ccb1c0b864c0822934b5e3d94f6d64c
msa <msai.sadehi@gmail.com>
2023-07-16 15:56:57 +0330

    turtle

--- session4.py
+++ session4.py
@@ -38,39 +38,60 @@
 # TODO   برنامه ای بنویسید که نام سه دانش آموز را از ورودی دریافت نماید و
 # آن ها را در لیستی ذخیره کند
 # students = []
-# name = input("enter your name: ")
-# students.append(name)
+# name1 = input("enter your name: ")
+# students.append(name1)
+#
+# name2 = input("enter a name: ")
+# students.append(name2)
+#
+# name3 = input("enter a name: ")
+# students.append(name3)
+#
 # print(students)
 
 
-numbers = [1, 2, 3, 4, 5, 6, 7]
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
 
 
 # print(numbers[:3])
 # print(numbers[:4])
 # print(numbers[1:4])
 
-# TODO    برنامه ای بنویسید که برش های زیر را از لیت بالا ایجاد نماید
+# TODO    برنامه ای بنویسید که برش های زیر را از لیست بالا ایجاد نماید
 
 # [1,2]
 # [3,4,5]
 # [6,7]
 
 
-# numbers = [1, 2, 3, 4, 5, 6, 7]
+numbers = [1, 2, 3, 4, 5, 6, 7]
+# print(numbers[0:2])
+# print(numbers[2:5])
+# print(numbers[5:7])
+
+
 
 # TODO  برنامه ای بنویسید که مجموع اعداد لیست بالا را نمایش دهد
+result  = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6]
+print(result)
+
+result = sum(numbers)
+print(result)
 
 
-favorite_sports = {}
-favorite_sports["sara"] = "tennis"
-favorite_sports["reza"] = "football"
-favorite_sports["matin"] = "football"
-favorite_sports["artin"] = "baseball"
 
-print(favorite_sports)
 
-print(f"sara likes {favorite_sports['sara']}")
-
-del favorite_sports["artin"]
-print(favorite_sports)
+# favorite_sports = {}
+# favorite_sports["sara"] = "tennis"
+# favorite_sports["reza"] = "football"
+# favorite_sports["matin"] = "football"
+# favorite_sports["artin"] = "baseball"
+#
+# print(favorite_sports)
+#
+# print(f"sara likes {favorite_sports['sara']}")
+#
+# del favorite_sports["artin"]
+# print(favorite_sports)
---
+++ session5.py
@@ -0,0 +1,21 @@
+import turtle
+
+main_screen = turtle.Screen()
+
+myturtle = turtle.Turtle()
+myturtle.color('red')
+myturtle.pensize(4)
+
+myturtle.forward(100)
+myturtle.left(90)
+
+myturtle.forward(100)
+myturtle.left(90)
+
+myturtle.forward(100)
+myturtle.left(90)
+
+myturtle.forward(100)
+myturtle.left(90)
+
+turtle.done()
