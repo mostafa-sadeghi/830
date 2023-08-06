10bf29d6996199193849fc6211261d205958d469
msa <msai.sadehi@gmail.com>
2023-08-06 16:57:29 +0330

    f

---
+++ session7.py
@@ -0,0 +1,14 @@
+# for i in range(6):
+#     print("hi")
+#     print("welcome")
+
+# print("outside")
+
+
+# for i in range(5):
+#     print(i)
+
+
+# for x in range(1, 11):
+#     print(x, end=" ")
+
