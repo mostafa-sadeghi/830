b044228e449f73c8d042a9463e18a3ebf19a66e3
msa <msai.sadeghi@gmail.com>
2023-08-17 15:56:23 +0330

    s

---
+++ session12.py
@@ -0,0 +1,34 @@
+name = "abtin"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+# print(name[-1])
+# print(name[-2:])
+# print(name[-3:])
+
+# x = str(123)
+# print(x[0])
+# print(x[1])
+# print(x[2])
+# print(x[-1])
+# print(x[-2:])
+
+
+# print(123 % 10)
+# print(123 % 100)
+
+
+"""
+تابعی بنویسید که سه عدد بگیرد و حاصل جمع را برگرداند
+
+"""
+
+
+def add(n1, n2, n3):
+    return n1 + n2 + n3
+
+
+print(add(3,6,8))
